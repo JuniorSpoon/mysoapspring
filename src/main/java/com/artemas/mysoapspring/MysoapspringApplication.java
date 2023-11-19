caa7f04755acfa3daf883ee54c016a94f636b675
artemvorobkin <avorobkin@gmail.com>
2023-11-19 13:59:21 +0300

    comment

---
+++ src/main/java/com/artemas/mysoapspring/MysoapspringApplication.java
@@ -0,0 +1,13 @@
+package com.artemas.mysoapspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysoapspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysoapspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/artemas/mysoapspring/MysoapspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artemas.mysoapspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysoapspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
